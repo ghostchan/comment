669cd30b232054e222160b1bb33d2f87197f8256
chenzhi03 <chenzhi03@zuche.com>
2017-06-29 11:38:01 +0800

    评论功能（三）

--- comment-app/src/Comment.js
+++ comment-app/src/Comment.js
@@ -1,3 +1,16 @@
-/**
- * Created by jinn on 2017/6/29.
- */
+import React, { Component } from 'react'
+
+class Comment extends Component {
+    render () {
+        return (
+            <div className='comment'>
+                <div className='comment-user'>
+                    <span>{this.props.comment.username} </span>：
+                </div>
+                <p>{this.props.comment.content}</p>
+            </div>
+        )
+    }
+}
+
+export default Comment
--- comment-app/src/CommentApp.js
+++ comment-app/src/CommentApp.js
@@ -3,8 +3,20 @@ import CommentInput from './CommentInput'
 import CommentList from './CommentList'
 
 class CommentApp extends Component {
+    constructor () {
+        super()
+        this.state = {
+            comments: []
+        }
+    }
     handleSubmitComment (comment) {
-        console.log(comment)
+        if (!comment) return;
+        if (!comment.username) return alert('请输入用户名');
+        if (!comment.content) return alert('请输入评论内容');
+        this.state.comments.push(comment);
+        this.setState({
+            comments:this.state.comments
+        });
     }
     render() {
         return (
@@ -12,7 +24,7 @@ class CommentApp extends Component {
             <CommentInput
                 onSubmit={this.handleSubmitComment.bind(this)}
             />
-            <CommentList />
+            <CommentList comments={this.state.comments}/>
             </div>
     )
     }
