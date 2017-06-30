b5d967d1fcf343fd9a2faaf887b0a878dfd2f4a6
chenzhi03 <chenzhi03@zuche.com>
2017-06-30 16:18:46 +0800

    评论功能（六）

--- comment-app/src/Comment.js
+++ comment-app/src/Comment.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 class Comment extends Component {
     static propTypes = {
-        comment: PropTypes.object.isRequired
+        comment: PropTypes.object.isRequired,
+        onDeleteComment: PropTypes.func,
+        index: PropTypes.number
     }
 
     constructor () {
         super()
         this.state = { timeString: '' }
     }
-
+    handleDeleteComment () {
+        if (this.props.onDeleteComment) {
+            this.props.onDeleteComment(this.props.index)
+        }
+    }
     componentWillMount () {
         this._updateTimeString();
         this._timer = setInterval(
@@ -27,16 +33,35 @@ class Comment extends Component {
                 : `${Math.round(Math.max(duration, 1))} 秒前`
         })
     }
-
+    _getProcessedContent (content) {
+        return content
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;")
+            .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+    }
+    componentWillUnmount () {
+        clearInterval(this._timer)
+    }
     render () {
         return (
             <div className='comment'>
                 <div className='comment-user'>
                     <span>{this.props.comment.username} </span>：
                 </div>
-                <p>{this.props.comment.content}</p>
+                <p dangerouslySetInnerHTML={{
+                    __html: this._getProcessedContent(this.props.comment.content)
+                }} />
                 <span className='comment-createdtime'>
                     {this.state.timeString}
+                </span>
+                <span
+                    className='comment-delete'
+                    onClick={this.handleDeleteComment.bind(this)}
+                >
+                    删除
                 </span>
             </div>
         )
